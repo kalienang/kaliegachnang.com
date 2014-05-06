fa1f66917d930d75827018e61322c2d9c5c8fbbd
Kalie Gachnang <kalie@kaliegachnang.com>
2014-05-05 19:29:27 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Kalie
+ * Date: 5/19/13
+ * Time: 10:47 AM
+ * To change this template use File | Settings | File Templates.
+ */
+$(document).ready();
